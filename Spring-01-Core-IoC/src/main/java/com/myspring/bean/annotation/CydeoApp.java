321f3d77b793d91bf288dce7cac335340351d8b9
kamilgurgener <kamilgurgener@gmail.com>
2023-02-19 16:38:59 -0500

    Multiple Beans Same Type were added.

--- Spring-01-Core-IoC/src/main/java/com/myspring/bean/annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/myspring/bean/annotation/CydeoApp.java
@@ -12,7 +12,9 @@ public class CydeoApp {
         FullTimeMentor ft = container.getBean(FullTimeMentor.class);
         ft.createAccount();
 
+//        PartTimeMentor pt = container.getBean("p2",PartTimeMentor.class);
         PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+
         pt.createAccount();
 
         String str = container.getBean(String.class);
