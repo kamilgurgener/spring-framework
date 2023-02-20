9ac9ac78a8452d6626bea4f6de22f31f54357148
kamilgurgener <kamilgurgener@gmail.com>
2023-02-20 17:27:43 -0500

    Added Dependency Injection

---
+++ Spring-03-Core-DI/src/main/java/com/mypsring/OfficeHours.java
@@ -0,0 +1,12 @@
+package com.mypsring;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours {
+
+    public int getHours(){
+        return 5;
+    }
+}
---
+++ Spring-03-Core-DI/src/main/java/com/mypsring/Selenium.java
@@ -0,0 +1,11 @@
+package com.mypsring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium {
+
+    public void getTeachingHours(){
+        System.out.println("Weekly teaching hours : 15");
+    }
+}
