60cc4460634ed2972c722e1caebd8e514208d38b
kamilgurgener <kamilgurgener@gmail.com>
2023-02-28 16:51:56 -0500

    Added Controller Class

--- Spring-07-MVC-Controller/src/main/java/com/myspring/ControllerApplication.java
+++ Spring-07-MVC-Controller/src/main/java/com/myspring/ControllerApplication.java
@@ -1,13 +1,14 @@
-package com.myspring.spring07mvccontroller;
+package com.myspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring07MvcControllerApplication {
+public class ControllerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring07MvcControllerApplication.class, args);
+
+		SpringApplication.run(ControllerApplication.class, args);
 	}
 
 }
