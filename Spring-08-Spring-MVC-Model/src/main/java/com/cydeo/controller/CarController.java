7e4b8119f9787c19fad290128604e1105604a933
kamilgurgener <kamilgurgener@gmail.com>
2023-03-07 11:41:42 -0500

    Added @PathVariable

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,12 +10,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/car")
 public class CarController {
 
-    @RequestMapping("/info")
-    public String carInfo(@RequestParam String make,@RequestParam int year, Model model){
+    @RequestMapping("/info")  //localhost:8080/car/info?make=honda
+    public String carInfo(@RequestParam  String make, @RequestParam Integer year, Model model){
 
         model.addAttribute("make", make);
-        model.addAttribute("year", 2021);
-        System.out.println(make);
+        model.addAttribute("year", year);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2")  //localhost:8080/car/info?make=honda
+    public String carInfo2(@RequestParam(value = "make", required=false,defaultValue = "Tesla")  String make, Model model){
+
+        model.addAttribute("make", make);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info/{make}/{year}")   ////localhost:8080/car/info/honda
+    public String getCarInfo(@PathVariable String make, @PathVariable Integer year, Model model){
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+
         return "car/car-info";
     }
 }
