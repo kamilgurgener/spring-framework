5a63e1e626cf43e4d0b3f6db9af58924af987af3
kamilgurgener <kamilgurgener@gmail.com>
2023-03-21 20:15:53 -0400

    Added Employee Registration Components

---
+++ My-Employee-Registration-Project/src/main/java/com/cydeo/myemployeeregistrationproject/model/Employee.java
@@ -0,0 +1,24 @@
+package com.cydeo.myemployeeregistrationproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+}
