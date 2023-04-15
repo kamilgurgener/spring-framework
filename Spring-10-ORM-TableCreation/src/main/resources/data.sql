a2949587c1e4574d00554842b76d0824fc14538e
kamilgurgener <kamilgurgener@gmail.com>
2023-04-15 14:47:17 -0400

    Added schema and data

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 
-INSERT INTO EMPLOYEES(id,name)
-VALUES(1,'Mike Smith');
+-- INSERT INTO EMPLOYEES(id,name)
+-- VALUES(1,'Mike Smith');
+
+INSERT INTO STUDENT(id,first_name,last_name,email)
+VALUES(1,'Mike', 'Smith', 'mike@some.com');
