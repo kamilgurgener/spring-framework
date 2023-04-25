e31bdeed0cf9156479d5f60b3900a3b43b9412ab
kamilgurgener <kamilgurgener@gmail.com>
2023-04-24 20:22:49 -0400

    Added Account Components

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,9 +1,16 @@
 package com.cydeo.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.math.BigDecimal;
 
+@MappedSuperclass // Parent class. Extended classes gets Class fields.
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //generates ID
     private Long id;
     private String owner;
     private BigDecimal balance;
--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/DebitAccount.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/DebitAccount.java
@@ -1,7 +1,10 @@
 package com.cydeo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 import java.math.BigDecimal;
 
+@Entity
 public class DebitAccount extends Account{
 
     private BigDecimal overDraftFee;
