514cb57e7dc7eef44504ba71c9f1438bc55e2b64
kamilgurgener <kamilgurgener@gmail.com>
2023-04-26 11:50:20 -0400

    ORM Part 2

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
