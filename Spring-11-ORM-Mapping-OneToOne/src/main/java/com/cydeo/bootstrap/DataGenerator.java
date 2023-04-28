2f3ed971fdcb06af4edfd01a52f32c45e2d3e17b
kamilgurgener <kamilgurgener@gmail.com>
2023-04-27 20:05:03 -0400

    ORM Part 3

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -3,6 +3,7 @@ package com.cydeo.bootstrap;
 import com.cydeo.entity.Department;
 import com.cydeo.entity.Employee;
 
+import com.cydeo.entity.Region;
 import com.cydeo.enums.Gender;
 import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.EmployeeRepository;
@@ -19,12 +20,11 @@ import org.springframework.boot.CommandLineRunner;
 public class DataGenerator implements CommandLineRunner {
 
     EmployeeRepository employeeRepository;
-    DepartmentRepository departmentRepository;
+//    DepartmentRepository departmentRepository;
 
 
-    public DataGenerator(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
+    public DataGenerator(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
-        this.departmentRepository = departmentRepository;
     }
 
     @Override
@@ -45,18 +45,29 @@ public class DataGenerator implements CommandLineRunner {
         Department d4 = new Department("Phones & Tablets", "Electronics");
         Department d5 = new Department("Computers", "Electronics");
 
+        Region r1 = new Region("Southwest","United States");
+        Region r2 = new Region("Central","United States");
+        Region r3 = new Region("Northwest","United States");
+        Region r4 = new Region("Quebec'","Canada");
+        Region r5 = new Region("Central","Asia");
 
-//        e1.setDepartment(d1);
-//        e2.setDepartment(d2);
-//        e3.setDepartment(d3);
-//        e4.setDepartment(d4);
-//        e5.setDepartment(d5);
+        e1.setDepartment(d1);
+        e2.setDepartment(d2);
+        e3.setDepartment(d3);
+        e4.setDepartment(d4);
+        e5.setDepartment(d5);
+
+        e1.setRegion(r1);
+        e2.setRegion(r2);
+        e3.setRegion(r3);
+        e4.setRegion(r4);
+        e5.setRegion(r5);
 
 
         employeeList.addAll(Arrays.asList(e1, e2, e3, e4, e5));
         departmentList.addAll(Arrays.asList(d1, d2, d3, d4, d5));
 
         employeeRepository.saveAll(employeeList);
-        departmentRepository.saveAll(departmentList);
+//        departmentRepository.saveAll(departmentList);
     }
 }
