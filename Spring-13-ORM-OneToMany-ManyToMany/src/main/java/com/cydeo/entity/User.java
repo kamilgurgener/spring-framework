3cc72c8052b9fe70fc2018d9ef26c133987b6fbb
kamilgurgener <kamilgurgener@gmail.com>
2023-05-01 20:54:34 -0400

    ORM ManyToMany

---
+++ Spring-13-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,24 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User extends BaseEntity{
+
+    private String email;
+    private String password;
+    private String username;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn (name = "account_details_id")
+    private Account account;
+
+}
