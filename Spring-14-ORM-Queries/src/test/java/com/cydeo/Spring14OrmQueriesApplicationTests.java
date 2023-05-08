b3389e350cc09976afb8143a3a45fc5f74a29299
kamilgurgener <kamilgurgener@gmail.com>
2023-05-08 08:27:13 -0400

    ORM Queries

--- Spring-14-ORM-Queries/src/test/java/com/cydeo/Spring14OrmQueriesApplicationTests.java
+++ Spring-14-ORM-Queries/src/test/java/com/cydeo/Spring14OrmQueriesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring14OrmQueriesApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
